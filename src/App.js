45e2f8d9cc1674fd6e7e4a248365a50e60a40cac
jinish08 <jinishshah08@gmailcom>
2021-10-06 20:03:56 +0530

    made profile responsive

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-// import Profile from "./Project/Profile/profile";
+import Profile from "./Project/Profile/profile";
 
 function App() {
   return (
     <div>
       <header className="flex h-screen w-screen">
-        <a
+        {/* <a
           className="m-auto text-5xl font-bold italic text-center"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Caretex Homepage
-        </a>
-        {/* <Profile /> */}
+        </a> */}
+        <Profile />
       </header>
     </div>
   );
