86b6e727e153af3d53b8e669fbaaac5fbc517c14
talkshrey <talkshrey@gmail.com>
2021-10-09 18:17:52 +0530

    Loading template added

--- src/Project/Homescreen/home.js
+++ src/Project/Homescreen/home.js
@@ -4,13 +4,12 @@ import ListIcon from '@material-ui/icons/List';
 import Menubar from '../../Shared/Menubar/menubar';
 import { Turn as Hamburger } from "hamburger-react";
 import { useState } from "react";
+import Skeleton from '@mui/material/Skeleton';
 import FeatherIcon from "feather-icons-react";
 
 export default function HomeScreen() {
   const [isOpen, setOpen] = useState(false);
   function Navbar() {
-
-
     return (
       <div className="z-20 w-full flex flex-col">
         <div className="flex flex-row justify-between h-24 md:py-8 py-4">
@@ -39,7 +38,9 @@ export default function HomeScreen() {
   function ProductCard(props) {
     return (
       <div className="rounded shadow-lg h-60 w-50 my-7 bg-white bg-opacity-20 hover:bg-gray-100 transform motion-safe hover:scale-105">
-        <div className="flex justify-center"> <img src="https://cdn-icons-png.flaticon.com/512/5032/5032177.png" className="mx-20 my-5 w-20 h-20" /> </div>
+        <div className="flex justify-center"> 
+        <img src="https://cdn-icons-png.flaticon.com/512/5032/5032177.png" className="mx-20 my-5 w-20 h-20" />
+        </div>
         <div className="font-bold text-lg text-center">Product Name</div>
         <p className="text-left mx-10 list-none">
           <li> Quantity </li>
@@ -49,12 +50,25 @@ export default function HomeScreen() {
     )
   }
 
+  function Loader() {
+    return (
+      <div className="rounded shadow-lg h-60 w-50 my-7 bg-white bg-opacity-20 hover:bg-gray-100 transform motion-safe hover:scale-105">
+        <div className="flex justify-center">
+          <div className="mt-5"> <Skeleton variant="rectangular" width={250} height={120}/> </div>
+        </div>
+        <div className="flex justify-center mt-3"> <Skeleton variant="text" width={250}/> </div>
+        <div className="flex justify-center"> <Skeleton variant="text" width={250}/> </div>
+      </div>
+    )
+  }
+
+
   return (
     <div className="w-full h-full bg-gradient-to-br from-blue-250 px-10 to-pink-200 overflow-auto">
       <Navbar />
       <img src="https://t3.ftcdn.net/jpg/03/20/67/68/360_F_320676834_dlqls0LHDYnMWm1CuAOuawn7rVXBiJ7B.jpg" className={"opacity-60 w-screen h-60 " + (isOpen ? "hidden" : "")} />
       <div className={"grid lg:grid-cols-3 gap-x-12 xs:grid-cols-1 sm:grid-cols-2 "+(isOpen ? "hidden" : "")}>
-        <div> <ProductCard /> </div>
+        <div> <Loader/> </div>
         <div> <ProductCard /> </div>
         <div> <ProductCard /> </div>
         <div> <ProductCard /> </div>
