ed4e76002d37977014e85a1d90f634488674efc8
talkshrey <talkshrey@gmail.com>
2021-10-18 21:52:51 +0530

    Package updated

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 
         </a>
         <HomeScreen/> */}
-        <Login/>
+        {/* <Login/> */}
         {/* <Profile/> */}
         {/* <Login/> */}
         {/* <Checkout /> */}
